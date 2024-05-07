172449ac979d867298fe3130ee52d5407288d2b2
itsme775 <kstowell77@gmail.com>
2024-05-07 13:32:44 -0400

    added test and code for reading

--- service/routes.py
+++ service/routes.py
@@ -67,9 +67,18 @@ def create_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_accounts(account_id):
+    """
+    This reads an account from the database, given account id
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
-# ... place you code here to READ an account ...
+    account=Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
 
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
